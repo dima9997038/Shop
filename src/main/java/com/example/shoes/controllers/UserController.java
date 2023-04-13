b7cdeacc207e23b064c1a7e1631b550f1f8483a1
dima9997038 <dima9997038@gmail.com>
2023-04-13 18:15:50 +0200

    Add admin

--- src/main/java/com/example/shoes/controllers/UserController.java
+++ src/main/java/com/example/shoes/controllers/UserController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -29,6 +30,12 @@ public class UserController {
             return "/registration";
         }
         return "/redirect:/login";
+    }
+    @GetMapping("/user/{user}")
+    public String userInfo(@PathVariable("user") User user, Model model) {
+        model.addAttribute("user", user);
+//        model.addAttribute("products", user.getProducts());
+        return "user-info";
     }
     @GetMapping("/hello")
     public String securityUrl(){
--- src/main/java/com/example/shoes/services/UserService.java
+++ src/main/java/com/example/shoes/services/UserService.java
@@ -8,6 +8,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @Slf4j
 @RequiredArgsConstructor
@@ -24,5 +26,23 @@ public class UserService {
         log.info("Saving new User with email: {}"+ email);
         userRepository.save(user);
         return true;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public void banUser(Long id) {
+        User user = userRepository.findById(id).orElse(null);
+        if (user != null) {
+            if (user.isActive()) {
+                user.setActive(false);
+                log.info("Ban user with id = {}; email: {}", user.getId(), user.getEmail());
+            } else {
+                user.setActive(true);
+                log.info("Unban user with id = {}; email: {}", user.getId(), user.getEmail());
+            }
+        }
+        userRepository.save(user);
     }
 }
